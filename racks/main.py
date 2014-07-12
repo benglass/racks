ed11128b156e5316b49f32e4dda0ad6b2cec9857
Ben Glassman <ben@vtdesignworks.com>
2014-07-12 01:06:18 -0400

    Add animation option.

--- racks/main.py
+++ racks/main.py
@@ -3,7 +3,7 @@
 """Racks.
 
 Usage:
-    racks rand [--count=<count>] [--min=<min>] [--max=<max>]
+    racks rand [--count=<count>] [--min=<min>] [--max=<max>] [--delay=<ms>]
     racks <numbers>...
 
 Examples:
@@ -15,10 +15,13 @@ Options:
     -c --count=<count>  Number of numbers to generate [default: 10]
     -n --min=<min>      Minimum number [default: 1]
     -x --max=<max>      Max number [default: 100]
+    -d --delay=<ms>     Delay for animation in ms
 """
 from docopt import docopt
 from termcolor import colored, cprint
 from random import randint
+import time
+import os
 
 steps = u'▁▂▃▄▅▆▇'
 
@@ -32,6 +35,17 @@ def normalize_numbers(numbers):
     rack = u''.join(steps[int(round((n - min(numbers)) / step))] for n in numbers)
     cprint(rack, 'green')
 
+def randomize_numbers(count, min, max):
+    numbers = [ randint(min, max) for x in range(count) ]
+    return numbers
+
+def animate_numbers(count, min, max, delay):
+    numbers = randomize_numbers(count, min, max)
+    normalize_numbers(numbers)
+    time.sleep(delay / float(1000))
+    os.system('clear')
+    animate_numbers(count, min, max, delay)
+
 def main():
     args = docopt(__doc__, version='1.0')
     numbers = args.get('<numbers>', None)
@@ -40,8 +54,13 @@ def main():
         ncount = int(args.get('--count'))
         nmin = int(args.get('--min'))
         nmax = int(args.get('--max'))
-        numbers = [ randint(nmin, nmax) for x in range(ncount) ]
-    if numbers:
+        delay = args.get('--delay')
+        if not delay:
+            numbers = randomize_numbers(ncount, nmin, nmax)
+            normalize_numbers(numbers)
+        else:
+            animate_numbers(ncount, nmin, nmax, int(delay))
+    elif numbers:
         try:
             numbers = map(int, numbers)
             normalize_numbers(numbers)
