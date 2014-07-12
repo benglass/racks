d26803cce6e969f1948017bdbab6f762de54ab71
Ben Glassman <ben@vtdesignworks.com>
2014-07-12 00:51:12 -0400

    Add random option.

--- racks/main.py
+++ racks/main.py
@@ -3,17 +3,22 @@
 """Racks.
 
 Usage:
+    racks rand [--count=<count>] [--min=<min>] [--max=<max>]
     racks <numbers>...
 
 Examples:
     racks 1 25 221 57 32 68 8
 
 Options:
-    -h --help     Show this screen.
-    -v --version  Show version.
+    -h --help           Show this screen.
+    -v --version        Show version.
+    -c --count=<count>  Number of numbers to generate [default: 10]
+    -n --min=<min>      Minimum number [default: 1]
+    -x --max=<max>      Max number [default: 100]
 """
 from docopt import docopt
 from termcolor import colored, cprint
+from random import randint
 
 steps = u'▁▂▃▄▅▆▇'
 
@@ -30,6 +35,12 @@ def normalize_numbers(numbers):
 def main():
     args = docopt(__doc__, version='1.0')
     numbers = args.get('<numbers>', None)
+    rand = args.get('rand', False)
+    if rand:
+        ncount = int(args.get('--count'))
+        nmin = int(args.get('--min'))
+        nmax = int(args.get('--max'))
+        numbers = [ randint(nmin, nmax) for x in range(ncount) ]
     if numbers:
         try:
             numbers = map(int, numbers)
